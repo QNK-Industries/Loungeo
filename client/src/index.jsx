133e7690002c4306919f66f675a58cb26a4840ac
xanzadu <noahjlehman@gmail.com>
2021-10-27 12:57:57 -0700

    fixing linting erors

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,3 +1,6 @@
+/* global document */
+/* eslint no-undef: "error" */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
+
 const app = express();
 const cors = require('cors');
+
 const PORT = 3000 || process.env.PORT;
 
 app.use(express.static('./client'));
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 app.listen(PORT, () => {
   // console.log('Server listening on port:', `${PORT}`);
