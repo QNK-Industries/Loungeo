1f2276731b130cf4f0dc9bd2d0c3e05ecf55c126
Khai Tran <kqtran917@yahoo.com>
2021-11-01 10:18:54 -0700

    Rendering widget to index

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,5 +3,6 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import QuestionsAnswers from './Questions_Answers/index.jsx'
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById('root'));
+ReactDOM.render(<QuestionsAnswers />, document.getElementById('root'));
