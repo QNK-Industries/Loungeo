d982730d9cc5cf44feac4ddb4fcf21a77b7e5695
Quinton Maki <quintonmaki@gmail.com>
2021-11-04 13:02:42 -0700

    Added foundational pieces to Ratings and Reviews

---
+++ client/src/Ratings_Reviews/components/RatingsTable.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const RatingsTable = () => (
+  <div>
+    Test
+  </div>
+);
+
+export default RatingsTable;
---
+++ client/src/Ratings_Reviews/components/ReviewSearch.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ReviewSearch = () => (
+  <div>
+    Test
+  </div>
+);
+
+export default ReviewSearch;
