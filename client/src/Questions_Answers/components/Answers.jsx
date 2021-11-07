1a447679b9f64cdff3244806be9b3032ee4cd052
Khai Tran <kqtran917@yahoo.com>
2021-11-07 14:17:56 -0600

    Implemented report answer

--- client/src/Questions_Answers/components/Answers.jsx
+++ client/src/Questions_Answers/components/Answers.jsx
@@ -7,18 +7,31 @@ const AnswerStyle = {
   marginTop: '15px',
 };
 
+const GH_TOKEN = require('../../../../tokens.js');
+
 const Answers = ({
   body, asker, date, helpful, id, addHelpful,
 }) => {
   const [questionId, setId] = useState(id);
   const putRequest = 'answers';
+
+  const reportAnswer = (reportId) => {
+    axios.put(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo/qa/answers/${reportId}/report`, {}, {
+      headers: {
+        Authorization: GH_TOKEN.GH_TOKEN,
+      },
+    })
+      .then((res) => console.log('Answer reported ', res.data))
+      .catch((error) => console.log(error));
+  };
+
   return (
   <div style={AnswerStyle}>
     <br />
     <strong>A</strong> {body}
     <br />
     <br />
-    By {asker}, {moment(date).utc().format('MMMM D, YYYY')} |  <span onClick={() => addHelpful(questionId, putRequest)}>Helpful? {helpful ? helpful : null}</span> | Report
+    By {asker}, {moment(date).utc().format('MMMM D, YYYY')} |  <span onClick={() => addHelpful(questionId, putRequest)}>Helpful? {helpful ? helpful : null}</span> | <span onClick={() => reportAnswer(questionId)}>Report</span>
   </div>
 );}
 
