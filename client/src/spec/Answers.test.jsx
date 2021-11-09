f24052902a000bbf01cf850f54575b97a089492e
Khai Tran <kqtran917@yahoo.com>
2021-11-09 17:16:40 -0600

    Implemented more tests for QA

--- client/src/spec/Answers.test.jsx
+++ client/src/spec/Answers.test.jsx
@@ -4,10 +4,30 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import Answers from '../Questions_Answers/components/Answers.jsx';
+import AnswerSearch from '../Questions_Answers/components/AnswerSearch.jsx';
+import Questions from '../Questions_Answers/components/Questions.jsx';
+import QuestionsAnswers from '../Questions_Answers/index.jsx';
 
-
-describe('Questions', () => {
+describe('Answers', () => {
   test('renders module component', () => {
     render(<Answers />);
+  });
+});
+
+describe('Questions', () => {
+  test('Should render Questions', () => {
+    render(<Questions />);
+  });
+});
+
+describe('Index', () => {
+  test('Should render index', () => {
+    render(<QuestionsAnswers />);
+  });
+});
+
+describe('Search', () => {
+  test('Should render Search', () => {
+    render(<AnswerSearch />);
   });
 });
