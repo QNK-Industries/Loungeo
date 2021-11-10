c396e1e6eae24a73dc5cd334100065edd59d909f
Quinton Maki <quintonmaki@gmail.com>
2021-11-09 17:56:00 -0800

    Achiever >70% test coverage on ratings and reviews

--- client/src/spec/JSONdata/reviews.js
+++ client/src/spec/JSONdata/reviews.js
@@ -24,5 +24,68 @@ module.exports.reviews = {
         },
       ],
     },
+    {
+      review_id: 1094575,
+      rating: 2,
+      summary: 'Example small review of product',
+      recommend: true,
+      response: null,
+      body: 'Example long review of product',
+      date: '2021-11-09T00:00:00.000Z',
+      reviewer_name: 'person',
+      helpfulness: 2,
+      photos: [
+        {
+          id: 2100375,
+          url: 'https://i.imgur.com/wl1S7do.jpeg',
+        },
+        {
+          id: 2100376,
+          url: 'https://i.imgur.com/xCeFAHu.jpeg',
+        },
+      ],
+    },
+    {
+      review_id: 1094575,
+      rating: 2,
+      summary: 'Example small review of product',
+      recommend: true,
+      response: null,
+      body: 'Example long review of product',
+      date: '2021-11-09T00:00:00.000Z',
+      reviewer_name: 'person',
+      helpfulness: 2,
+      photos: [
+        {
+          id: 2100375,
+          url: 'https://i.imgur.com/wl1S7do.jpeg',
+        },
+        {
+          id: 2100376,
+          url: 'https://i.imgur.com/xCeFAHu.jpeg',
+        },
+      ],
+    },
+    {
+      review_id: 1094575,
+      rating: 2,
+      summary: 'Example small review of product',
+      recommend: true,
+      response: null,
+      body: 'Example long review of product',
+      date: '2021-11-09T00:00:00.000Z',
+      reviewer_name: 'person',
+      helpfulness: 2,
+      photos: [
+        {
+          id: 2100375,
+          url: 'https://i.imgur.com/wl1S7do.jpeg',
+        },
+        {
+          id: 2100376,
+          url: 'https://i.imgur.com/xCeFAHu.jpeg',
+        },
+      ],
+    },
   ],
 };
--- client/src/spec/RatingsAndReviews.test.jsx
+++ client/src/spec/RatingsAndReviews.test.jsx
@@ -4,6 +4,7 @@ import {
   screen,
   act,
   waitFor,
+  fireEvent,
 } from '@testing-library/react';
 import {
   test,
@@ -33,5 +34,55 @@ describe('Ratings and Reviews', () => {
   test('renders Review widget to DOM', async () => {
     const appContainer = await waitFor(() => screen.getByTestId('ratingstable'));
     expect(appContainer).toBeInTheDocument();
+  });
+
+  test('displays the modal on button click', async () => {
+    const button = await waitFor(() => screen.getByText(/add a review/i));
+    fireEvent.click(button);
+    const modal = await waitFor(() => screen.getByText(/write your review/i));
+    expect(modal).toBeVisible();
+  });
+
+  test('form modal contains summary forms', async () => {
+    const button = await waitFor(() => screen.getByText(/add a review/i));
+    fireEvent.click(button);
+    const summary = await waitFor(() => screen.getByPlaceholderText(/best purchase/i));
+    expect(summary).toBeVisible();
+  });
+
+  test('form modal contains filled summary forms', async () => {
+    const button = await waitFor(() => screen.getByText(/add a review/i));
+    fireEvent.click(button);
+    const summary = await waitFor(() => screen.getByPlaceholderText(/best purchase/i));
+    fireEvent.change(summary, { target: { value: 'test' } });
+    expect(summary.value).toBe('test');
+  });
+
+  test('form modal contains body forms', async () => {
+    const button = await waitFor(() => screen.getByText(/add a review/i));
+    fireEvent.click(button);
+    const body = await waitFor(() => screen.getByPlaceholderText(/why did you like/i));
+    expect(body).toBeVisible();
+  });
+
+  test('form modal contains filled summary forms', async () => {
+    const button = await waitFor(() => screen.getByText(/add a review/i));
+    fireEvent.click(button);
+    const body = await waitFor(() => screen.getByPlaceholderText(/why did you like/i));
+    fireEvent.change(body, { target: { value: 'test' } });
+    expect(body.value).toBe('test');
+  });
+
+  test('displays more reviews on button click', async () => {
+    const button = await waitFor(() => screen.getByText(/more reviews/i));
+    fireEvent.click(button);
+    const modal = await waitFor(() => screen.getAllByText(/report/i)[3]);
+    expect(modal).toBeVisible();
+  });
+
+  test('search bar works', async () => {
+    const search = await waitFor(() => screen.getByPlaceholderText(/search reviews/i));
+    fireEvent.change(search, { target: { value: 'test test test nothing here' } });
+    expect(search.value).toBe('test test test nothing here');
   });
 });
