9ef2cbcfb380936601cc533e00ed1366fa5f0c0a
Khai Tran <kqtran917@yahoo.com>
2021-11-22 15:48:41 -0600

    Implemented sdc server integration

--- server/apiconfig.js
+++ server/apiconfig.js
@@ -2,13 +2,13 @@
 const baseURL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo';
 // for SDC services
 const productServiceURL = 'insert your product service URL from SDC here';
-const qaServiceURL = 'insert your question and answer service URL from SDC here';
+const qaServiceURL = 'localhost:8080';
 const reviewServiceURL = 'insert your review service URL from SDC here';
 
 if (process.env.API_MODE === 'individual') {
   // fix your individual service URL here and set the rest to baseURL
   module.exports.productURL = 'insert productServiceURL or baseURL';
-  module.exports.qaURL = 'insert qaServiceURL or baseURL';
+  module.exports.qaURL = qaServiceURL;
   module.exports.reviewURL = 'insert reviewServiceURL or baseURL';
 }
 
